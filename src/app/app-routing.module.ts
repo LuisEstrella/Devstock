ca31ab1c45d6f28ae0cb3acb2ed6f6e07523b0c0
LuisEstrella <73119745+LuisEstrella@users.noreply.github.com>
2021-04-12 15:19:47 -0500

    Base Iniciar
    :+1:

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StockcontentComponent } from './components/stockcontent/stockcontent.component';
+import { DevmainComponent } from './main/devmain/devmain.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: DevmainComponent},
+  {path:'stock', component: StockcontentComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
